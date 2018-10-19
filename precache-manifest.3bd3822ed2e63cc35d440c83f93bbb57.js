3113c37608c41a65eb9f215f1b7afb698e636436
berniris <berniriscoleman@gmail.com>
2018-10-19 12:38:09 -0400

    Updates

---
+++ precache-manifest.3bd3822ed2e63cc35d440c83f93bbb57.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8a2d7ed765dcbc5c1928cdede9dc9b9",
+    "url": "/reliable-weather/static/media/pisa.e8a2d7ed.png"
+  },
+  {
+    "revision": "2e2f6a175331dab6011f",
+    "url": "/reliable-weather/static/css/main.140a549d.chunk.css"
+  },
+  {
+    "revision": "56b961ec01a53466fa8f",
+    "url": "/reliable-weather/static/js/1.56b961ec.chunk.js"
+  },
+  {
+    "revision": "5f541af0e7993aef24ee",
+    "url": "/reliable-weather/static/js/runtime~main.5f541af0.js"
+  },
+  {
+    "revision": "7d8c0f8646e2953ffc941bcfd4b250b1",
+    "url": "/reliable-weather/static/media/paris.7d8c0f86.png"
+  },
+  {
+    "revision": "1f0392a742bd89ec49f44de02f35b95e",
+    "url": "/reliable-weather/static/media/nyc.1f0392a7.png"
+  },
+  {
+    "revision": "2e2f6a175331dab6011f",
+    "url": "/reliable-weather/static/js/main.2e2f6a17.chunk.js"
+  },
+  {
+    "revision": "f3bde50003bb54416a4d66c27a5f6aea",
+    "url": "/reliable-weather/static/media/amsterdam.f3bde500.png"
+  },
+  {
+    "revision": "ca537327af56279976dd2c5c9f59e214",
+    "url": "/reliable-weather/static/media/barcelona.ca537327.png"
+  },
+  {
+    "revision": "8fb12c359e4abbb55186588b2549ef0c",
+    "url": "/reliable-weather/static/media/Millennium-Round.8fb12c35.otf"
+  },
+  {
+    "revision": "50d88d12b3a0a5e10b97cb65c6884719",
+    "url": "/reliable-weather/static/media/Nord Bold.50d88d12.otf"
+  },
+  {
+    "revision": "3a0f9633ddc2d24cb9b798098ca49731",
+    "url": "/reliable-weather/static/media/Nord Medium.3a0f9633.otf"
+  },
+  {
+    "revision": "283ac0221b2fb148b464391a81420388",
+    "url": "/reliable-weather/index.html"
+  }
+];
