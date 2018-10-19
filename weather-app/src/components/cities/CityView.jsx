e44e9e76fc3539b453aa67d79fc81534d47b6737
berniris <berniriscoleman@gmail.com>
2018-10-19 01:48:21 -0400

    Added more styling to CityVIew component including an exit button

--- weather-app/src/components/cities/CityView.jsx
+++ weather-app/src/components/cities/CityView.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function CityView(props) {
-	console.log(props)
+	const cityImg = props.city.name.toLowerCase().replace(' ', '-') + `-` + `img`;
 	return (
 	<div className="view-container">
 		<div className="view-header"><h1>{`${props.city.name}`}</h1>
@@ -16,13 +17,18 @@ function CityView(props) {
 			</div>
 			<div className="description">
                 <img src={`http://openweathermap.org/img/w/${props.city.weather[0].icon}.png`} alt="weather-icon" />
-                <p><span className="detail">Today's weather in {props.city.name}:</span> {props.city.weather[0].description}</p>
+                <p><span className="detail">Current weather in {props.city.name}:</span> {props.city.weather[0].description}</p>
             </div>
             <div className="weather-details">
             <p><span className="detail">Low:</span> <span>{Math.floor(props.city.main.temp_min)}&deg;F</span></p>
             <p><span className="detail">High:</span> <span>{Math.floor(props.city.main.temp_max)}&deg;F</span></p>
         </div>
         </div>
+        <div className={`${cityImg} city-img`} alt="city-icon" />
+        <div className="skinny-rect-shape"></div>
+        <div className="circle-shape"></div>
+        <div className="view-background"></div>
+        <Link to="/"><div className="close"></div></Link>
 		</div>
 		)
 }
