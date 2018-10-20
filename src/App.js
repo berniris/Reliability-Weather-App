5853cea6ea3172b66b767e3b60d1c5d20a1cd6dd
berniris <berniriscoleman@gmail.com>
2018-10-20 04:04:06 -0400

    xyz

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import View from './components/cities/View'
